64caf1a4f8ddf0e6408b1b8c45deea01b0a902c2
ThTddd <thtddd@Mac.lan>
2026-03-06 12:11:16 +0100

    task: l'utilisateur peut rentrer une adresse

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css'; // Obligatoire pour l'affichage des tuiles
 
@@ -16,9 +17,39 @@ L.Marker.prototype.options.icon = DefaultIcon;
 
 function App() {
   const position: [number, number] = [48.8566, 2.3522]; // Coordonnées de Paris
+  const [search, setSearch ] = useState("");
 
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          alert("Recherche lancée pour : " + search);
+        }}
+        style={{
+          position: 'absolute',
+          top: '20px',
+          left: '50%',
+          transform: 'translateX(-50%)',
+          zIndex: 1000,
+          background: 'white',
+          padding: '10px',
+          borderRadius: '8px',
+          boxShadow: '0 2px 5px rgba(0,0,0,0.2)'
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Entrez une destination..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+            style={{ padding: '8px', border: '1px solid #ccc', borderRadius: '4px', width: '250px' }}
+        />
+      <button type="submit" style={{ marginLeft: '5px', padding: '8px 15px', cursor: 'pointer'}}>
+        Rechercher
+      </button>
+      </form>
+
       <MapContainer 
         center={position} 
         zoom={13} 
